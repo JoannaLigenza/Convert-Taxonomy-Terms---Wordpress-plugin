5c5bc5e5dd65b96250e65894386aff58a297759a
JoannaLigenza <ligenzajoanna@gmail.com>
2020-05-25 13:08:16 +0200

    initial commit

---
+++ jl-convert-taxonomy-terms.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: JL Convert Taxonomy Terms
+ * Description: JL Convert Taxonomy Terms plugin allows to move or copy taxonomy terms and its children to another taxonomy with saving taxonomy hierarchy
+ * Version: 1.0.
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ * Author: JL
+ * Text Domain: jlconverttax
+ * Domain Path: /languages
+ * License: GPL2
+ * 
+ * JL Convert Taxonomy Terms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *   
+ * JL Convert Taxonomy Terms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with JL Convert Taxonomy Terms. If not, see {URI to Plugin License}.
+ */
+
+
+defined( 'ABSPATH' ) or die( 'hey, you don\'t have an access to read this site' );
+
+
---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Trigger this file when user uninstall plugin
+ * 
+ * @package jlconverttax
+ *
+*/
+
+// security check - prevent access from outside of wordpress
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
+}
+
+
