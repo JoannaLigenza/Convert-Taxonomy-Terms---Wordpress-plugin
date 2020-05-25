a2dce9fd7ebb34948c2ad79af55adb6b86492302
JoannaLigenza <ligenzajoanna@gmail.com>
2020-05-25 18:15:41 +0200

    added cleaning function

--- uninstall.php
+++ uninstall.php
@@ -11,4 +11,14 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     die;
 }
 
+function jlconverttax_delete_settings() {
+    delete_option( 'jlconverttax-save-hierarchy' );
+    delete_option( 'jlconverttax-from-taxonomy' );
+    delete_option( 'jlconverttax-to-taxonomy' );
+    delete_option( 'jlconverttax-checked-categories' );
+}
+
+jlconverttax_delete_settings();
+
+
 
