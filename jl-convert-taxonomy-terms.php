98b2447a824f7d9c51e4a47b1dc6da64a2a08587
JoannaLigenza <ligenzajoanna@gmail.com>
2020-05-25 18:16:13 +0200

    changed plugin licency info

--- jl-convert-taxonomy-terms.php
+++ jl-convert-taxonomy-terms.php
@@ -1,18 +1,19 @@
 <?php
 /**
  * Plugin Name: JL Convert Taxonomy Terms
- * Description: JL Convert Taxonomy Terms plugin allows to move or copy taxonomy terms and its children to another taxonomy with saving taxonomy hierarchy
+ * Description: JL Convert Taxonomy Terms plugin allows to move taxonomy terms and its children to another taxonomy with saving taxonomy hierarchy
  * Version: 1.0.
  * Requires at least: 5.0
  * Requires PHP: 7.0
- * Author: JL
+ * Author: JL-lovecoding
+ * Author URI: https://love-coding.pl/en
  * Text Domain: jlconverttax
  * Domain Path: /languages
- * License: GPL2
+ * License: GPLv3
  * 
  * JL Convert Taxonomy Terms is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *   
  * JL Convert Taxonomy Terms is distributed in the hope that it will be useful,
@@ -21,7 +22,7 @@
  * GNU General Public License for more details.
  
  * You should have received a copy of the GNU General Public License
- * along with JL Convert Taxonomy Terms. If not, see {URI to Plugin License}.
+ * along with JL Convert Taxonomy Terms. If not, see http://www.gnu.org/licenses/gpl.html.
  */
 
 
